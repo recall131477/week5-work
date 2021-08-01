286d79c2b9736fa2475d22e6bf31b5689c61ecbe
rousong <recall131477@gmail.com>
2021-08-02 00:23:23 +0800

    過濾tailwind多餘class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  // purge: {
-  //   enabled: true,
-  //   content: ['./app/**/*.html'],
-  // },
+  purge: {
+    enabled: true,
+    content: ['./app/**/*.html'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
